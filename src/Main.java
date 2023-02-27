80e67f47f9b25e00c25422582b52482829352b21
IlyuAfanasev <gecktor.86@mail.ru>
2023-02-27 16:42:24 +0400

    Task 6

--- src/Main.java
+++ src/Main.java
@@ -5,36 +5,39 @@ public class Main {
         task3();
         task4();
         task5();
+        task6();
     }
 
     public static void task1 () {
         System.out.println("     Задача 1");
         for ( int i=1; i<=10; i++) {
-            System.out.println(i);
+            System.out.print(i+" ");
         }
+        System.out.println();
     }
 
     public static void task2 () {
         System.out.println("     Задача 2");
         for ( int i=10; i>=1; i--) {
-            System.out.println(i);
+            System.out.print(i+" ");
         }
-
+        System.out.println();
     }
     public static void task3 () {
         System.out.println("     Задача 3");
         for ( int i=0; i<=17; i++) {
             if (i%2==0) {
-                System.out.println(i);
+                System.out.print(i+" ");
             }
         }
+        System.out.println();
     }
     public static void task4 () {
         System.out.println("     Задача 4");
         for ( int i=10; i>=-10; i--) {
-            System.out.println(i);
+            System.out.print(i+" ");
         }
-
+        System.out.println();
     }
     public static void task5 () {
         System.out.println("     Задача 5");
@@ -42,6 +45,13 @@ public class Main {
             System.out.println(i+ " год является високосным");
         }
 
+    }
+    public static void task6 () {
+        System.out.println("     Задача 6");
+        for ( int i=7; i<=98; i=i+7) {
+            System.out.print(i+" ");
+        }
+        System.out.println();
     }
 
 }
